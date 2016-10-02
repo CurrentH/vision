1ff3e8e515bbb0a41b0620eecbb110746dca3f71
Theis <rorissian@hotmail.com>
2016-10-02 14:40:11 +0200

    Mini project with images, and other

---
+++ mini_project/build/CMakeFiles/image1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/theis/workspace/vision/mini_project/src/image1.cpp" "/home/theis/workspace/vision/mini_project/build/CMakeFiles/image1.dir/home/theis/workspace/vision/mini_project/src/image1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mini_project/src/image1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+
+int main(int argc, char** argv )
+{
+	Mat image; 
+	Mat dst;
+	image = cv::imread("../images/Image1.png", CV_LOAD_IMAGE_GRAYSCALE);
+
+	medianBlur ( image, dst, 11 );
+
+	if ( !image.data )
+	{
+		printf("No image data \n");
+		return -1;
+	}
+
+	namedWindow("Display Image", CV_WINDOW_NORMAL );
+	imshow("Display Image", dst);
+
+	waitKey(0);
+
+	return 0;
+}
